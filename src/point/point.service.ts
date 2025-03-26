806d6a15e8ea046ef25d65438fe2d5d107bb6b1e
psyoongsc <psyoongsc@naver.com>
2025-03-26 15:53:56 +0900

    Refactor: 동시 요청 제어를 위한 Mutex 반영
    유저 별 Mutex 적용으로 동시 요청 제어

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -3,9 +3,12 @@ import { PointHistoryTable } from "../database/pointhistory.table";
 import { Inject, Injectable } from "@nestjs/common";
 import { PointHistory, TransactionType, UserPoint } from "./point.model";
 import { ConfigService } from "@nestjs/config";
+import { Mutex } from "async-mutex";
 
 @Injectable()
 export class PointService {
+  private locks = new Map<number, Mutex>(); // 유저 별 동시성 제어
+
   constructor(
     private readonly userDb: UserPointTable,
     private readonly historyDb: PointHistoryTable,
@@ -13,6 +16,15 @@ export class PointService {
   ) {}
 
   private readonly chargeLimit: number = Number(this.config.get<string>("CHARGE_LIMIT", "100000"));
+
+  private getMutexForUser(userId: number): Mutex {
+    // Mutex lock chain 에 등록
+    if (!this.locks.has(userId)) {
+      this.locks.set(userId, new Mutex());
+    }
+
+    return this.locks.get(userId);
+  }
 
   // TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
   async getPoint(userId: number): Promise<UserPoint> {
@@ -42,17 +54,20 @@ export class PointService {
       throw new Error("충전할 포인트는 정수여야 하며 0보다 커야합니다.");
     }
 
+    const mutex = this.getMutexForUser(userId);
     let updatedPoint;
-    const userPoint = await this.userDb.selectById(userId);
-    updatedPoint = userPoint.point + amount;
+    await mutex.runExclusive(async () => {
+      const userPoint = await this.userDb.selectById(userId);
+      updatedPoint = userPoint.point + amount;
 
-    // NOTE Module 에서 CHARGE_LIMIT 을 주입한 값을 사용한다.
-    if (updatedPoint > this.chargeLimit) {
-      throw new Error(`충전할 수 있는 최대 포인트는 ${this.chargeLimit} 입니다.`);
-    }
+      // NOTE Module 에서 CHARGE_LIMIT 을 주입한 값을 사용한다.
+      if (updatedPoint > this.chargeLimit) {
+        throw new Error(`충전할 수 있는 최대 포인트는 ${this.chargeLimit} 입니다.`);
+      }
 
-    await this.userDb.insertOrUpdate(userId, updatedPoint);
-    await this.historyDb.insert(userId, amount, TransactionType.CHARGE, Date.now());
+      await this.userDb.insertOrUpdate(userId, updatedPoint);
+      await this.historyDb.insert(userId, amount, TransactionType.CHARGE, Date.now());
+    });
 
     return { id: userId, point: updatedPoint, updateMillis: Date.now() };
   }
@@ -67,16 +82,19 @@ export class PointService {
       throw new Error("사용할 포인트는 정수여야 하며 0보다 커야합니다.");
     }
 
+    const mutex = this.getMutexForUser(userId);
     let updatedPoint;
-    const userPoint = await this.userDb.selectById(userId);
-    updatedPoint = userPoint.point - amount;
+    await mutex.runExclusive(async () => {
+      const userPoint = await this.userDb.selectById(userId);
+      updatedPoint = userPoint.point - amount;
 
-    if (updatedPoint < 0) {
-      throw new Error("포인트가 부족합니다.");
-    }
+      if (updatedPoint < 0) {
+        throw new Error("포인트가 부족합니다.");
+      }
 
-    await this.userDb.insertOrUpdate(userId, updatedPoint);
-    await this.historyDb.insert(userId, amount, TransactionType.USE, Date.now());
+      await this.userDb.insertOrUpdate(userId, updatedPoint);
+      await this.historyDb.insert(userId, amount, TransactionType.USE, Date.now());
+    });
 
     return { id: userId, point: updatedPoint, updateMillis: Date.now() };
   }
