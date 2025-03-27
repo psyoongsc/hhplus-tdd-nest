434d80defda80002345c9fe21c326549630a9fcb
psyoongsc <psyoongsc@naver.com>
2025-03-27 22:15:57 +0900

    Refactor: 포인트 서비스 코드 최적화(Mutex Ver)
    1. 충전, 사용 로직 내에서 updatedPoint 를 let 으로 선언하였지만 초기화 후 재할당이 없어야 하기에  const 로 수정하였음
    2. 충전, 사용 로직 내에서 최종 반환하는 UserPoint 객체를 실제 userDb 에 저장한 객체로 반환하도록 수정하였음 (Mutex 외부에서 return 하기 위해서 let 변수를 사용함)

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -17,6 +17,12 @@ export class PointService {
 
   private readonly chargeLimit: number = Number(this.config.get<string>("CHARGE_LIMIT", "100000"));
 
+  private validateUserId(userId: number) {
+    if (!Number.isInteger(userId) || userId <= 0) {
+      throw new Error("올바르지 않은 ID 값 입니다.");
+    }
+  }
+
   private getMutexForUser(userId: number): Mutex {
     // Mutex lock chain 에 등록
     if (!this.locks.has(userId)) {
@@ -28,74 +34,66 @@ export class PointService {
 
   // TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
   async getPoint(userId: number): Promise<UserPoint> {
-    if (!Number.isInteger(userId) || userId <= 0) {
-      throw new Error("올바르지 않은 ID 값 입니다.");
-    }
+    this.validateUserId(userId);
 
     return await this.userDb.selectById(userId);
   }
 
   // TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
   async getHistories(userId: number): Promise<PointHistory[]> {
-    if (!Number.isInteger(userId) || userId <= 0) {
-      throw new Error("올바르지 않은 ID 값 입니다.");
-    }
+    this.validateUserId(userId);
 
     return await this.historyDb.selectAllByUserId(userId);
   }
 
   // TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
   async chargePoint(userId: number, amount: number): Promise<UserPoint> {
-    if (!Number.isInteger(userId) || userId <= 0) {
-      throw new Error("올바르지 않은 ID 값 입니다.");
-    }
+    this.validateUserId(userId);
 
     if (!Number.isInteger(amount) || amount <= 0) {
       throw new Error("충전할 포인트는 정수여야 하며 0보다 커야합니다.");
     }
 
     const mutex = this.getMutexForUser(userId);
-    let updatedPoint;
+    let chargedUserPoint: UserPoint;
     await mutex.runExclusive(async () => {
       const userPoint = await this.userDb.selectById(userId);
-      updatedPoint = userPoint.point + amount;
+      const updatedPoint = userPoint.point + amount;
 
       // NOTE Module 에서 CHARGE_LIMIT 을 주입한 값을 사용한다.
       if (updatedPoint > this.chargeLimit) {
         throw new Error(`충전할 수 있는 최대 포인트는 ${this.chargeLimit} 입니다.`);
       }
 
-      await this.userDb.insertOrUpdate(userId, updatedPoint);
+      chargedUserPoint = await this.userDb.insertOrUpdate(userId, updatedPoint);
       await this.historyDb.insert(userId, amount, TransactionType.CHARGE, Date.now());
     });
 
-    return { id: userId, point: updatedPoint, updateMillis: Date.now() };
+    return chargedUserPoint;
   }
 
   // TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
   async usePoint(userId: number, amount: number): Promise<UserPoint> {
-    if (!Number.isInteger(userId) || userId <= 0) {
-      throw new Error("올바르지 않은 ID 값 입니다.");
-    }
+    this.validateUserId(userId);
 
     if (!Number.isInteger(amount) || amount <= 0) {
       throw new Error("사용할 포인트는 정수여야 하며 0보다 커야합니다.");
     }
 
     const mutex = this.getMutexForUser(userId);
-    let updatedPoint;
+    let usedUserPoint: UserPoint;
     await mutex.runExclusive(async () => {
       const userPoint = await this.userDb.selectById(userId);
-      updatedPoint = userPoint.point - amount;
+      const updatedPoint = userPoint.point - amount;
 
       if (updatedPoint < 0) {
         throw new Error("포인트가 부족합니다.");
       }
 
-      await this.userDb.insertOrUpdate(userId, updatedPoint);
+      usedUserPoint = await this.userDb.insertOrUpdate(userId, updatedPoint);
       await this.historyDb.insert(userId, amount, TransactionType.USE, Date.now());
     });
 
-    return { id: userId, point: updatedPoint, updateMillis: Date.now() };
+    return usedUserPoint;
   }
 }
